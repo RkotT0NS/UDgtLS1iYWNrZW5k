d98ea847da7953a2ef1623f8a431727fbc296a00
Fabien Jacq <tetedacier@proton.me>
2026-06-03 18:14:30 +0200

    ci: Simplify build and test jobs

--- build.sh
+++ build.sh
@@ -2,78 +2,29 @@
 
 set -e
 
-CHECK_PROJECT() {
-    GRADLE_PROJECT=false
-    NPM_PROJECT=false
-
-    if [ -f "settings.gradle" ]; then
-        GRADLE_PROJECT=true
-        echo "GRADLE";
-    fi
-
-    if [ -f "package.json" ]; then
-        NPM_PROJECT=true
-        echo "NPM";
-    fi
-
-    if [[ ! ( $GRADLE_PROJECT == "true"  || $NPM_PROJECT == "true") ]]; then
-        echo "Neither Gradle, NPM, nor Maven project found";
-        exit 1;
-    fi
-}
-
-BUILD_PROJECT() {
-    IMAGE_NAME="${1:-}"
-    if [ -z "$IMAGE_NAME" ]; then
-        echo "IMAGE_NAME is not set"
-        exit 1
-    fi
-
-    # Shift the first argument to get any additional tags
-    shift
-    ADDITIONAL_TAGS=("$@")
-
-    # Color codes for pretty output
-    GREEN='\033[0;32m'
-    BLUE='\033[0;34m'
-    NC='\033[0m' # No Color
-
-    echo -e "${BLUE}===> Building Docker production image (target: prod) for ${IMAGE_NAME}...${NC}"
-
-    # Construct the build command with all tags
-    BUILD_CMD=("docker" "build" ${DOCKER_BUILD_PARAMS:-} "--target" "prod" "-t" "${IMAGE_NAME}" ".")
-    for TAG in "${ADDITIONAL_TAGS[@]}"; do
-        BUILD_CMD+=("-t" "${TAG}")
-    done
-
-    # Run the docker build command
-    "${BUILD_CMD[@]}"
-
-    echo -e "${GREEN}===> Build completed successfully!${NC}"
-}
-
-PROJECT_TYPE="$(CHECK_PROJECT)";
-PROJECT_FOUND="$?";
-if [ $PROJECT_FOUND -ne 0 ]; then
-    exit $PROJECT_FOUND;
+IMAGE_NAME="${1:-}"
+if [ -z "$IMAGE_NAME" ]; then
+    echo "IMAGE_NAME is not set"
+    exit 1
 fi
 
-# The script accepts tags passed as arguments
-# e.g., ./build.sh tag1 tag2 ...
-# If no arguments are passed, it will use default names
-if [[ "$PROJECT_TYPE" =~ (^|$'\n')GRADLE($|$'\n') ]]; then
-    echo "Gradle project detected"
-    if [ $# -gt 0 ]; then
-        BUILD_PROJECT "$@"
-    else
-        BUILD_PROJECT "p8-backend"
-    fi
+IMAGE_VERSION="${2:-}"
+if [ -z "$IMAGE_VERSION" ]; then
+    echo "IMAGE_VERSION is not set"
+    exit 2
 fi
 
-if [[ "$PROJECT_TYPE" =~ (^|$'\n')NPM($|$'\n') ]]; then
-    if [ $# -gt 0 ]; then
-        BUILD_PROJECT "$@"
-    else
-        BUILD_PROJECT "p8-frontend"
-    fi
-fi
+# Color codes for pretty output
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}===> Building Docker production image (target: prod) for ${IMAGE_NAME}...${NC}"
+
+# Construct the build command with all tags
+docker build ${DOCKER_BUILD_PARAMS:-} --target prod -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+
+# Run the docker build command
+# "${BUILD_CMD}"
+
+echo -e "${GREEN}===> Build completed successfully!${NC}"
--- test.sh
+++ test.sh
@@ -2,71 +2,39 @@
 
 set -e
 
-CHECK_PROJECT() {
-    GRADLE_PROJECT=false
-    NPM_PROJECT=false
-
-    if [ -f "settings.gradle" ]; then
-        GRADLE_PROJECT=true
-        echo "GRADLE";
-    fi
-
-    if [ -f "package.json" ]; then
-        NPM_PROJECT=true
-        echo "NPM";
-    fi
-
-    if [[ ! ( $GRADLE_PROJECT == "true"  || $NPM_PROJECT == "true") ]]; then
-        echo "Neither Gradle, NPM, nor Maven project found";
-        exit 1;
-    fi
-}
-
-TEST_PROJECT() {
-    IMAGE_NAME="${1:-}"
-    if [ -z "$IMAGE_NAME" ]; then
-        echo "IMAGE_NAME is not set"
-        exit 1
-    fi
-
-    # Color codes for pretty output
-    GREEN='\033[0;32m'
-    BLUE='\033[0;34m'
-    NC='\033[0m' # No Color
-
-    echo -e "${BLUE}===> Building Docker test image (target: test)...${NC}"
-    docker build ${DOCKER_BUILD_PARAMS:-} --target test -t "${IMAGE_NAME}" .
-
-    echo -e "${BLUE}===> Running project tests inside Docker container...${NC}"
-    CONTAINER_NAME="test-runner-${IMAGE_NAME}"
-
-    # Run tests; do not exit immediately on failure so we can extract reports
-    set +e
-    docker run --rm  --volume "$(pwd)/test-results:/app/test-results" --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
-    TEST_EXIT_CODE=$?
-    set -e
-
-    # Exit with the test suite's exit code if they failed
-    if [ $TEST_EXIT_CODE -ne 0 ]; then
-        echo -e "\033[0;31m===> Tests failed with exit code ${TEST_EXIT_CODE}!\033[0m"
-        exit $TEST_EXIT_CODE
-    fi
-
-    echo -e "${GREEN}===> Tests completed successfully!${NC}"
-}
-
-PROJECT_TYPE="$(CHECK_PROJECT)";
-PROJECT_FOUND="$?";
-if [ $PROJECT_FOUND -ne 0 ]; then
-    exit $PROJECT_FOUND;
+IMAGE_NAME="${1:-}"
+if [ -z "$IMAGE_NAME" ]; then
+    echo "IMAGE_NAME is not set"
+    exit 1
 fi
 
-# cleanup test results
-rm -rf test-results/*.xml
+IMAGE_VERSION="${2:-}"
+if [ -z "$IMAGE_VERSION" ]; then
+    echo "IMAGE_VERSION is not set"
+    exit 2
+fi
 
-if [[ "$PROJECT_TYPE" =~ (^|$'\n')GRADLE($|$'\n') ]]; then
-    TEST_PROJECT "p8-backend-test"
+# Color codes for pretty output
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}===> Building Docker test image (target: test)...${NC}"
+docker build ${DOCKER_BUILD_PARAMS:-} --target test -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
+
+echo -e "${BLUE}===> Running project tests inside Docker container...${NC}"
+CONTAINER_NAME="test-runner-${IMAGE_NAME}"
+
+# Run tests; do not exit immediately on failure so we can extract reports
+set +e
+docker run --rm  --volume "$(pwd)/test-results:/app/test-results" --name "${CONTAINER_NAME}" "${IMAGE_NAME}:${IMAGE_VERSION}"
+TEST_EXIT_CODE=$?
+set -e
+
+# Exit with the test suite's exit code if they failed
+if [ $TEST_EXIT_CODE -ne 0 ]; then
+    echo -e "\033[0;31m===> Tests failed with exit code ${TEST_EXIT_CODE}!\033[0m"
+    exit $TEST_EXIT_CODE
 fi
-if [[ "$PROJECT_TYPE" =~ (^|$'\n')NPM($|$'\n') ]]; then
-    TEST_PROJECT "p8-frontend-test"
-fi
+
+echo -e "${GREEN}===> Tests completed successfully!${NC}"
