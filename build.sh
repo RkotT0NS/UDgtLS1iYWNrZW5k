4ee3e05183b5dd3352a348ce185ef91c4da772e8
Fabien Jacq <tetedacier@proton.me>
2026-06-04 10:27:29 +0200

    ci: Restore project detection

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+source "scripts/project-detection.source"
+source "scripts/color.source"
+
 IMAGE_NAME="${1:-}"
 if [ -z "$IMAGE_NAME" ]; then
     echo "IMAGE_NAME is not set"
@@ -14,17 +17,35 @@ if [ -z "$IMAGE_VERSION" ]; then
     exit 2
 fi
 
-# Color codes for pretty output
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+BUILD_PROJECT() {
+    PROJECT_NAME="${1:-}"
+    IMAGE_NAME="${2:-}"
+    IMAGE_VERSION="${3:-}"
+    echo -e "${BLUE}===> Building Docker production image (target: prod) for ${PROJECT_NAME}...${NC}"
 
-echo -e "${BLUE}===> Building Docker production image (target: prod) for ${IMAGE_NAME}...${NC}"
+    # Construct the build command with all tags
+    docker build \
+        ${DOCKER_BUILD_PARAMS:-} \
+        --target prod \
+        -t \
+        ${IMAGE_NAME}:${IMAGE_VERSION} .
 
-# Construct the build command with all tags
-docker build ${DOCKER_BUILD_PARAMS:-} --target prod -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+    # Run the docker build command
+    # "${BUILD_CMD}"
 
-# Run the docker build command
-# "${BUILD_CMD}"
+    echo -e "${GREEN}===> Build completed successfully!${NC}"
+}
 
-echo -e "${GREEN}===> Build completed successfully!${NC}"
+PROJECT_TYPE="$(CHECK_PROJECT)";
+PROJECT_FOUND="$?";
+if [ $PROJECT_FOUND -ne 0 ]; then
+    exit $PROJECT_FOUND;
+fi
+
+if [[ "$PROJECT_TYPE" =~ (^|$'\n')GRADLE($|$'\n') ]]; then
+    TEST_PROJECT "wd34-backend" "${PROJECT_NAME}" "${IMAGE_NAME}" "${IMAGE_VERSION}"
+fi
+
+if [[ "$PROJECT_TYPE" =~ (^|$'\n')NPM($|$'\n') ]]; then
+    TEST_PROJECT "wd34-frontend" "${PROJECT_NAME}" "${IMAGE_VERSION}"
+fi
--- test.sh
+++ test.sh
@@ -2,39 +2,61 @@
 
 set -e
 
-IMAGE_NAME="${1:-}"
-if [ -z "$IMAGE_NAME" ]; then
-    echo "IMAGE_NAME is not set"
-    exit 1
-fi
+source "scripts/project-detection.source"
+source "scripts/color.source"
 
-IMAGE_VERSION="${2:-}"
+IMAGE_VERSION="${1:-}"
 if [ -z "$IMAGE_VERSION" ]; then
     echo "IMAGE_VERSION is not set"
     exit 2
 fi
 
-# Color codes for pretty output
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
 
-echo -e "${BLUE}===> Building Docker test image (target: test)...${NC}"
-docker build ${DOCKER_BUILD_PARAMS:-} --target test -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
+TEST_PROJECT() {
+    PROJECT_NAME="${1:-}"
+    IMAGE_VERSION="${2:-}"
 
-echo -e "${BLUE}===> Running project tests inside Docker container...${NC}"
-CONTAINER_NAME="test-runner-${IMAGE_NAME}"
+    echo -e "${BLUE}===> Building Docker test image (target: test) for ${PROJECT_NAME}...${NC}"
+    docker build \
+        ${DOCKER_BUILD_PARAMS:-} \
+        --target test \
+        -t \
+        "${PROJECT_NAME}:${IMAGE_VERSION}" .
 
-# Run tests; do not exit immediately on failure so we can extract reports
-set +e
-docker run --rm  --volume "$(pwd)/test-results:/app/test-results" --name "${CONTAINER_NAME}" "${IMAGE_NAME}:${IMAGE_VERSION}"
-TEST_EXIT_CODE=$?
-set -e
+    echo -e "${BLUE}===> Running project tests inside Docker container...${NC}"
+    CONTAINER_NAME="test-${PROJECT_NAME}"
 
-# Exit with the test suite's exit code if they failed
-if [ $TEST_EXIT_CODE -ne 0 ]; then
-    echo -e "\033[0;31m===> Tests failed with exit code ${TEST_EXIT_CODE}!\033[0m"
-    exit $TEST_EXIT_CODE
+    # Run tests; do not exit immediately on failure so we can extract reports
+    set +e
+    docker run \
+        --rm  \
+        --volume "$(pwd)/test-results:/app/test-results" \
+        --name "${CONTAINER_NAME}" \
+        "${PROJECT_NAME}:${IMAGE_VERSION}"
+
+    TEST_EXIT_CODE=$?
+    set -e
+
+    # Exit with the test suite's exit code if they failed
+    if [ $TEST_EXIT_CODE -ne 0 ]; then
+        echo -e "\033[0;31m===> Tests failed with exit code ${TEST_EXIT_CODE}!\033[0m"
+        exit $TEST_EXIT_CODE
+    fi
+
+    echo -e "${GREEN}===> Tests completed successfully!${NC}"
+}
+
+
+PROJECT_TYPE="$(CHECK_PROJECT)";
+PROJECT_FOUND="$?";
+if [ $PROJECT_FOUND -ne 0 ]; then
+    exit $PROJECT_FOUND;
 fi
 
-echo -e "${GREEN}===> Tests completed successfully!${NC}"
+if [[ "$PROJECT_TYPE" =~ (^|$'\n')GRADLE($|$'\n') ]]; then
+    TEST_PROJECT "wd34-backend" "${IMAGE_VERSION}"
+fi
+
+if [[ "$PROJECT_TYPE" =~ (^|$'\n')NPM($|$'\n') ]]; then
+    TEST_PROJECT "wd34-frontend" "${IMAGE_VERSION}"
+fi
